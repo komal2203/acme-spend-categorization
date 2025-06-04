159ba153e54a40ab3d0601f1f1ebf3aeab41e7ab
Komal Meena <capkomal2203@gmail.com>
2025-06-04 07:11:49 +0530

    update

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,9 @@
-bind = "0.0.0.0:10000" 
-timeout = 300
-workers = 2
+# gunicorn_config.py
+bind = "0.0.0.0:10000"
+timeout = 600  # 10 minutes
+workers = 1
 worker_class = 'sync'
 keepalive = 5
+max_requests = 100
+max_requests_jitter = 10
+graceful_timeout = 120
