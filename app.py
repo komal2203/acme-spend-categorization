9b0bee35b91d5af12001f03189b36983cb161ef9
Komal Meena <capkomal2203@gmail.com>
2025-06-04 05:53:56 +0530

    update

--- app.py
+++ app.py
@@ -316,5 +316,11 @@ def index():
         uploaded_filename=uploaded_filename
     )
 
+# if __name__ == "__main__":
+#     app.run(debug=False)
+
+
+# Add at the end of app.py
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
